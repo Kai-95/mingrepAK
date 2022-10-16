521e34e646243b0a69ef3784e8e2bdd35b4cd372
kai-95 <kagorilla9@gmail.com>
2022-10-16 22:08:49 +0900

    first commit

---
+++ target/debug/deps/minigrep-7e9d8ec14cf70eb3.d
@@ -0,0 +1,5 @@
+/home/kai/Documents/project_rust/minigrep/target/debug/deps/minigrep-7e9d8ec14cf70eb3: src/main.rs
+
+/home/kai/Documents/project_rust/minigrep/target/debug/deps/minigrep-7e9d8ec14cf70eb3.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/kai/Documents/project_rust/minigrep/target/debug/minigrep: /home/kai/Documents/project_rust/minigrep/src/main.rs
